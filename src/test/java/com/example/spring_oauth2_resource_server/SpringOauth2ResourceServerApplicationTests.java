0e1a7adfe0d9795f118134ac40b1cb8955bdcb52
kimjihun <xxxjjhhh@naver.com>
2024-10-11 20:08:28 +0900

    * init

---
+++ src/test/java/com/example/spring_oauth2_resource_server/SpringOauth2ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_oauth2_resource_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2ResourceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
